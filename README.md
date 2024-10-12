6fad6736f42b26c7792363de00393fc1c3520fd1
Ole-Jakob Olsen <ole.jakob.olsen@ngi.no>
2024-10-12 13:34:28 +0200

    Adding tailwind styling

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # HTMX-HTPY Presentation
 
+## Requirements
+
+- Tailwind CLI
+
+
 ## Develop
 
 `uvicorn src.htmxhtpy.main:app --host 0.0.0.0 --port 8080 --reload`
+
+# Styling
+
+`tailwindcss -i src/htmxhtpy/tailwind.css -o static/styles.css --watch`
 
 ## Deploy
 
--- src/htmxhtpy/main.py
+++ src/htmxhtpy/main.py
@@ -1,4 +1,7 @@
+import htpy as h
 from fastapi import FastAPI
+from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -6,3 +9,21 @@ app = FastAPI()
 @app.get("/ping")
 def ping():
     return "Pong"
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("static/favicon.ico")
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+def landing():
+    return HTMLResponse(
+        h.html[
+            h.head[h.link(rel="stylesheet", href="/static/styles.css")],
+            h.body[h.p(class_="w-screen h-screen bg-red-500")["Hello there!"],],
+        ]
+    )
